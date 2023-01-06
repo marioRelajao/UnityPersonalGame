fe0af35c4f117ab7dcbe799a849cf170371a2dcf
marioRelajao <mario.cerda@estudiantat.upc.edu>
2023-01-06 02:25:09 +0100

    Tenemos unidades de colision

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,10 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] int movementSpeed; //Podemos acceder a el desde el inspector, pero otros scripts no
 
+    [SerializeField] Rigidbody2D playerRigidbody;
+
     private Vector2 movementInput; //Vector de 2 dimensiones [X,Y] para el movimiento
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +21,7 @@ public class PlayerController : MonoBehaviour
     {
         movementInput.x = Input.GetAxisRaw("Horizontal"); //El eje X se guia por el input llamado "Horizontal"
         movementInput.y = Input.GetAxisRaw("Vertical");
-        transform.position += new Vector3(movementInput.x, movementInput.y, 0f)*Time.deltaTime*movementSpeed;
+        //transform.position += new Vector3(movementInput.x, movementInput.y, 0f)*Time.deltaTime*movementSpeed; //El deltatime es necesario para ser independientes del framerate
+        playerRigidbody.velocity = movementInput * movementSpeed;
     }
 }
