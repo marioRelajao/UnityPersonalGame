0ac621cfdd6ab8ae8a970c0c68d1fa6bcbb6238b
marioRelajao <mario.cerda@estudiantat.upc.edu>
2023-01-06 19:38:54 +0100

    Mi man esta animado 🤙Merequetengue🤙

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] Transform weaponArm; //Apuntar arma
 
+    private Animator playerAnimator;
     private Camera mainCamera;
     private Vector2 movementInput; //Vector de 2 dimensiones [X,Y] para el movimiento
     
@@ -17,20 +18,45 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         mainCamera = Camera.main; //Referenciamos a la camara principal
+        playerAnimator = GetComponent<Animator>(); //Ahora tenemos referencia al Animator del Player
     }
 
     // Update is called once per frame
     void Update()
     {
+        //----------------------------------MOVIMIENTO DEL JUGADOR----------------------------------
         movementInput.x = Input.GetAxisRaw("Horizontal"); //El eje X se guia por el input llamado "Horizontal"
         movementInput.y = Input.GetAxisRaw("Vertical");
         playerRigidbody.velocity = movementInput * movementSpeed;
 
+        //----------------------------------ROTAR ARMA----------------------------------
         Vector3 mousePos = Input.mousePosition;
         Vector3 screenPoint = mainCamera.WorldToScreenPoint(transform.localPosition); //Punto de la componente Transform de ese objeto
 
         Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
         float angulo = Mathf.Atan2(offset.y,offset.x)*Mathf.Rad2Deg; //Retorna rad, necesitamos grados
         weaponArm.rotation = Quaternion.Euler(0, 0, angulo);
+
+        //----------------------------------ROTAR SPRITE----------------------------------
+        if (mousePos.x < screenPoint.x)//Miramos a la izquierda
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            weaponArm.localScale = new Vector3(-1f, -1f, 1f);
+        }
+        else//Reiniciamos la orientacion
+        {
+            transform.localScale = Vector3.one;//Seteamos todo a 1
+            weaponArm.localScale = Vector3.one;
+        }
+
+        //----------------------------------BOOL ANIM----------------------------------
+        if (movementInput != Vector2.zero)
+        {
+            playerAnimator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("IsWalking", false);
+        }
     }
 }
