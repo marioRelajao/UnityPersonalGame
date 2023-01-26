d78c21badbb49e1f969c8b909069ff1604e7bce5
marioRelajao <mario.cerda@estudiantat.upc.edu>
2023-01-26 23:45:39 +0100

    Lista de enemigos en el nivel, pero creo que he roto algo de los colliders del SwordShoot

--- Assets/Scripts/Managers/RoomManager.cs
+++ Assets/Scripts/Managers/RoomManager.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class RoomManager : MonoBehaviour
 {
-    [SerializeField] GameObject[] doorsToClose;
-    [SerializeField] bool closeDoorOnPlayerEnter;
+    [SerializeField] GameObject[] doorsToClose; //Array con las puertas a abrir
+    [SerializeField] bool closeDoorOnPlayerEnter, openDoorsNoEnemies;  //Bool que necesito para las condiciones
+    [SerializeField] List<Collider2D> enemies = new List<Collider2D>(); //Lista de enemigos a comprobar 
+
+    private Collider2D roomCollider;  //Collider para controlar cantidad enemigos/abir puertas
+    private ContactFilter2D contactFilter2D; //Filtrar para poder hacer el Overlap
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Get del collider, creamos la mascara y obtenemos array de enemigos
+        roomCollider = GetComponent<Collider2D>();
+        contactFilter2D.SetLayerMask(LayerMask.GetMask("Enemy"));
+        //Mirar la documentacion de OverlapCollider, dada una mascara te da los elementos en una array
+        roomCollider.OverlapCollider(contactFilter2D, enemies);
     }
 
     // Update is called once per frame
